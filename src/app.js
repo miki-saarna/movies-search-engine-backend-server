9881b009e5ac6f6a9e38e1e2c4f11acfd8489dc2
Mikito Saarna <mikitosaarna@gmail.com>
2021-11-11 19:40:33 -0600

    Began working on adding error tracing logger - has issues

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
+// const logger = require("./config/logger")
 
 // require error handler
 const notFound = require("./errors/notFound")
@@ -14,6 +16,8 @@ const theatersRouter = require('./theaters/theaters.router');
 const app = express();
 
 app.use(morgan("dev"));
+// app.use(logger);
+app.use(cors());
 // needed to read body from req.body...
 app.use(express.json());
 
