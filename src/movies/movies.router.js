a811c722d81d93739932acc8dd77521c41a9da4c
Mikito Saarna <mikitosaarna@gmail.com>
2021-11-11 19:01:47 -0600

    Included methodNotAllowed handler on all routes

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const controller = require("./movies.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed")
 
-router.route("/").get(controller.list);
+router.route("/").get(controller.list).all(methodNotAllowed);
 
-router.route("/:movieId").get(controller.findMovie);
+router.route("/:movieId").get(controller.findMovie).all(methodNotAllowed);
 
-router.route("/:movieId/theaters").get(controller.findTheatersShowingMovie);
+router.route("/:movieId/theaters").get(controller.findTheatersShowingMovie).all(methodNotAllowed);
 
-router.route("/:movieId/reviews").get(controller.findReviewsForMovie);
+router.route("/:movieId/reviews").get(controller.findReviewsForMovie).all(methodNotAllowed);
 
 module.exports = router;
